f61654681ec361217321d311696396869d5de241
wlan0 <sidharthamn@gmail.com>
2020-07-21 22:13:21 -0700

    fix gofmt

--- pkg/volume/factory.go
+++ pkg/volume/factory.go
@@ -21,7 +21,7 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
-	
+
 	"github.com/golang/glog"
 )
 
@@ -41,13 +41,13 @@ func InitializeFactory(paths []string) {
 func Provision(volumeID string) (string, error) {
 	provisionerLock.Lock()
 	defer provisionerLock.Unlock()
-	
+
 	if len(vf.Paths) == 0 {
 		return "", fmt.Errorf("no base paths provided for directs")
 	}
 	next := vf.LastAssigned + 1
 	next = next % len(vf.Paths)
-	
+
 	nextPath := vf.Paths[next]
 	glog.V(15).Infof("[%s] using direct storage: BasePaths[%d] = %s", volumeID, next, nextPath)
 
