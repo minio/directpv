30a28dfda352f08d71d94a44385f5d782f9e6635
Harshavardhana <harsha@minio.io>
2021-01-28 21:38:41 -0800

    update multi-arch builds

--- package.sh
+++ package.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 binary=$(basename "$(dirname "${1}")")
-minisign -s "/media/${USER}/minio/minisign.key" \
-         -x "${binary}.minisig" \
-         -Sm "${1}" < "/media/${USER}/minio/minisign-passphrase"
-
-cp -f "${binary}.minisig" "${1}.minisig"
-cp -f LICENSE "$(dirname "${1}")"
-zip -r -j "${binary}.zip" "$(dirname "${1}")"
-
-if [ -f direct-csi_linux_amd64.minisig ]; then
-    mv direct-csi_linux_amd64.minisig direct-csi.minisig
-fi
-
-if [ -f kubectl-direct_csi_linux_amd64.minisig ]; then
-    mv kubectl-direct_csi_linux_amd64.minisig kubectl-direct_csi.minisig
+if [[ "${binary}" =~ kubectl-direct_csi* ]]; then
+    cp -f LICENSE "$(dirname "${1}")"
+    cp -f CREDITS "$(dirname "${1}")"
+    cp -f README.md  "$(dirname "${1}")"
+    zip -q -r -j "${binary}.zip" "$(dirname "${1}")"
 fi
