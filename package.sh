4cd8feb662a57ec2b706da2736277d1ca5afed9f
Harshavardhana <harsha@minio.io>
2021-02-04 15:38:16 -0800

    migrate to quay.io for direct-csi image

--- package.sh
+++ package.sh
@@ -5,5 +5,6 @@ if [[ "${binary}" =~ kubectl-direct_csi* ]]; then
     cp -f LICENSE "$(dirname "${1}")"
     cp -f CREDITS "$(dirname "${1}")"
     cp -f README.md  "$(dirname "${1}")"
+    rm -f "${binary}.zip"
     zip -q -r -j "${binary}.zip" "$(dirname "${1}")"
 fi
