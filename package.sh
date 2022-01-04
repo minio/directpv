16c84134071c6b59b78bd450ef6b5cce13d493fd
Harshavardhana <harsha@minio.io>
2022-01-04 15:30:52 -0800

    update package.sh to package kubectl-directpv as well

--- package.sh
+++ package.sh
@@ -7,4 +7,10 @@ if [[ "${binary}" =~ kubectl-direct_csi* ]]; then
     cp -f README.md  "$(dirname "${1}")"
     rm -f "${binary}.zip"
     zip -q -r -j "${binary}.zip" "$(dirname "${1}")"
+elif [[ "${binary}" =~ kubectl-directpv* ]]; then
+    cp -f LICENSE "$(dirname "${1}")"
+    cp -f CREDITS "$(dirname "${1}")"
+    cp -f README.md  "$(dirname "${1}")"
+    rm -f "${binary}.zip"
+    zip -q -r -j "${binary}.zip" "$(dirname "${1}")"
 fi
