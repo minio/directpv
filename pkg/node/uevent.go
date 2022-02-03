39335f77d788e5a017c12d730066ccd8fa525cad
Praveenrajmani <praveen@minio.io>
2022-02-03 19:30:15 +0530

    Implement Add

--- pkg/node/uevent.go
+++ pkg/node/uevent.go
@@ -18,8 +18,11 @@ package node
 
 import (
 	"context"
+	"errors"
 	"path"
-	
+
+	"github.com/google/uuid"
+
 	directcsi "github.com/minio/directpv/pkg/apis/direct.csi.min.io/v1beta3"
 	"github.com/minio/directpv/pkg/client"
 	"github.com/minio/directpv/pkg/sys"
@@ -28,11 +31,15 @@ import (
 	"k8s.io/klog/v2"
 )
 
+var (
+	errNoMatchFound = errors.New("no matching drive found")
+)
+
 func RunDynamicDriveHandler(ctx context.Context,
 	identity, nodeID, rack, zone, region string,
 	loopbackOnly bool) error {
 
-	handler := &uEventHandler{
+	handler := &driveEventHandler{
 		nodeID: nodeID,
 		topology: map[string]string{
 			string(utils.TopologyDriverIdentity): identity,
@@ -41,17 +48,10 @@ func RunDynamicDriveHandler(ctx context.Context,
 			string(utils.TopologyDriverRegion):   region,
 			string(utils.TopologyDriverNode):     nodeID,
 		},
-		loopbackOnly: loopbackOnly,
-	}
-	if loopbackOnly {
-		if err := sys.CreateLoopDevices(); err != nil {
-			return err
-		}
+		createDevice: sys.CreateDevice,
 	}
 
-	listener, err := uevent.NewListener(&driveEventHandler{
-		nodeID: nodeID,
-	})
+	listener, err := uevent.NewListener(handler)
 	if err != nil {
 		return err
 	}
@@ -61,16 +61,14 @@ func RunDynamicDriveHandler(ctx context.Context,
 }
 
 type driveEventHandler struct {
-	nodeID string
+	nodeID       string
+	topology     map[string]string
+	createDevice func(event map[string]string) (device *sys.Device, err error)
 }
 
 func (d *driveEventHandler) remove(ctx context.Context,
 	device *sys.Device,
-	drives *directcsi.DirectCSIDrive) error {
-
-	for _, drive := range drives {
-
-	}
+	drive *directcsi.DirectCSIDrive) error {
 
 	return nil
 }
@@ -81,19 +79,28 @@ func (d *driveEventHandler) update(ctx context.Context,
 
 	// path - ?
 	// ...
-	
-	
+
 	return nil
 }
 
-func (d *driveEventHandler) add(ctx context.Context,
+func (d *driveEventHandler) add(
+	ctx context.Context,
 	device *sys.Device) error {
-
-	// construct directcsiDrive object here and push it to etcd
-	
+	drive := client.NewDirectCSIDrive(
+		uuid.New().String(),
+		client.NewDirectCSIDriveStatus(device, d.nodeID, d.topology),
+	)
+	err := client.CreateDrive(ctx, drive)
+	if err != nil {
+		klog.ErrorS(err, "unable to create drive", "Status.Path", drive.Status.Path)
+	}
+	return err
 }
 
-func (d *driveEventHandler) findMatchingDrive() {
+func (d *driveEventHandler) findMatchingDrive(drives []directcsi.DirectCSIDrive, device *sys.Device) (*directcsi.DirectCSIDrive, error) {
+	//  todo: run matching algorithm to find matching drive
+	//  note: return `errNoMatchFound` if no match is found
+	return nil, errNoMatchFound
 }
 
 func (d *driveEventHandler) Handle(ctx context.Context, device *sys.Device) error {
@@ -106,39 +113,42 @@ func (d *driveEventHandler) Handle(ctx context.Context, device *sys.Device) erro
 		return nil
 	}
 
-	device, err := sys.CreateDevice(event)
+	device, err := d.createDevice(event)
 	if err != nil {
 		klog.ErrorS(err, "ACTION", event["ACTION"], "DEVPATH", event["DEVPATH"])
 		return nil
 	}
 
-	driveCh, err := client.ListDrives(
+	drives, err := client.GetDriveList(
 		ctx,
 		[]utils.LabelValue{utils.NewLabelValue(d.nodeID)},
 		[]utils.LabelValue{utils.NewLabelValue(device.Name)},
 		nil,
-		client.MaxThreadCount,
 	)
 	if err != nil {
-		klog.ErrorS(err, "error while finding matching drive")
+		klog.ErrorS(err, "error while fetching drive list")
 		return err
 	}
 
-	drives := []*directcsi.DirectCSIDrive{}
-	for drive := range driveCh {
-		drives = append(drives, drive)
-	}
-	
-	if len(drives) == 0 {
-		klog.V(5).Infof("no matching DirectPV drive found", "device", device.Name)
-		// code to handle new drive
-	} else {
+	drive, err := d.findMatchingDrive(drives, device)
+	switch {
+	case errors.Is(err, errNoMatchFound):
+		if event["ACTION"] == uevent.Remove {
+			klog.V(3).InfoS(
+				"matching drive not found",
+				"ACTION", uevent.Remove,
+				"DEVPATH", event["DEVPATH"])
+			return nil
+		}
+		return d.add(ctx, device)
+	case err == nil:
 		switch event["ACTION"] {
 		case uevent.Remove:
 			return d.remove(ctx, device, drive)
 		default:
 			return d.update(ctx, device, drive)
 		}
+	default:
+		return err
 	}
-	return nil
 }
