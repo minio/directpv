82a85420d3ffe290fc4d451f5f15f35575de89b2
Bala.FA <bala@minio.io>
2022-02-10 17:43:53 +0530

    rename eventQueue to eventStore

--- pkg/uevent/queue.go
+++ pkg/uevent/queue.go
@@ -49,15 +49,15 @@ func (d deviceEvent) String() string {
 		d.created, d.devPath, d.action)
 }
 
-type eventQueue struct {
+type eventStore struct {
 	events map[string]*deviceEvent
 	mutex  sync.Mutex
 	keyCh  chan string
 	keys   sync.Map
 }
 
-func newEventQueue() *eventQueue {
-	return &eventQueue{
+func newEventStore() *eventStore {
+	return &eventStore{
 		events: map[string]*deviceEvent{},
 		keyCh:  make(chan string, 16384),
 	}
@@ -76,7 +76,7 @@ func newEventQueue() *eventQueue {
 //   q.push(ev)
 // }
 //
-func (e *eventQueue) push(event *deviceEvent) {
+func (e *eventStore) push(event *deviceEvent) {
 	e.mutex.Lock()
 	defer e.mutex.Unlock()
 
@@ -125,7 +125,7 @@ func (e *eventQueue) push(event *deviceEvent) {
 	e.events[event.devPath] = event
 }
 
-func (e *eventQueue) pop() *deviceEvent {
+func (e *eventStore) pop() *deviceEvent {
 	key := <-e.keyCh
 
 	e.mutex.Lock()
