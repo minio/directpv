84eb3f2a5715da969ddf3b401f5a03e8461b1e91
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2023-05-23 06:14:58 -0600

    Add supported OC version for Red Hat CSI Certification (#772)

--- olm.sh
+++ olm.sh
@@ -10,6 +10,9 @@ operator-sdk generate bundle \
     --channels stable \
     --overwrite
 
+# Annotations to specify OCP versions compatibility.
+yq -i '.annotations."com.redhat.openshift.versions" |= "v4.8-v4.12"' bundles/redhat-marketplace/"$RELEASE"/metadata/annotations.yaml
+
 # Add needed annotations for redhat marketplace
 yq -i '.metadata.annotations."marketplace.openshift.io/remote-workflow" |= "https://marketplace.redhat.com/en-us/operators/minio-directpv-operator-rhmp/pricing?utm_source=openshift_console"' bundles/redhat-marketplace/"$RELEASE"/manifests/minio-directpv.clusterserviceversion.yaml
 yq -i '.metadata.annotations."marketplace.openshift.io/support-workflow" |= "https://marketplace.redhat.com/en-us/operators/minio-directpv-operator-rhmp/support?utm_source=openshift_console"' bundles/redhat-marketplace/"$RELEASE"/manifests/minio-directpv.clusterserviceversion.yaml
