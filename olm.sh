59a80b8846b0bd6abf9863b252597f24201d4293
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2023-06-06 17:42:20 -0400

    Using redhat containers in olm script

--- olm.sh
+++ olm.sh
@@ -20,15 +20,15 @@ yq -i '.metadata.annotations."marketplace.openshift.io/remote-workflow" |= "http
 yq -i '.metadata.annotations."marketplace.openshift.io/support-workflow" |= "https://marketplace.redhat.com/en-us/operators/minio-directpv-operator-rhmp/support?utm_source=openshift_console"' bundles/redhat-marketplace/"$RELEASE"/manifests/$package.clusterserviceversion.yaml
 
 # Use SHA Digest for CSI Provisioner Image
-csiProvisionerImage="quay.io/minio/csi-provisioner:v3.4.0"
+csiProvisionerImage="registry.redhat.io/openshift4/ose-csi-external-provisioner:latest"
 csiProvisionerImageDigest=$(podman pull "$csiProvisionerImage" | awk '/Digest/ { print $2}')
-csiProvisionerImageDigest="quay.io/minio/csi-provisioner@${csiProvisionerImageDigest}"
+csiProvisionerImageDigest="registry.redhat.io/openshift4/ose-csi-external-provisioner@${csiProvisionerImageDigest}"
 yq -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].image |= (\"${csiProvisionerImageDigest}\")" bundles/redhat-marketplace/"$RELEASE"/manifests/"$package".clusterserviceversion.yaml
 
 # Use SHA Digest for CSI Resizer Image
-csiResizerImage="quay.io/minio/csi-resizer:v1.7.0"
+csiResizerImage="registry.redhat.io/openshift4/ose-csi-external-resizer-rhel8:latest"
 csiResizerImageDigest=$(podman pull "$csiResizerImage" | awk '/Digest/ { print $2}')
-csiResizerImageDigest="quay.io/minio/csi-resizer@${csiResizerImageDigest}"
+csiResizerImageDigest="registry.redhat.io/openshift4/ose-csi-external-resizer-rhel8@${csiResizerImageDigest}"
 yq -i ".spec.install.spec.deployments[0].spec.template.spec.containers[1].image |= (\"${csiResizerImageDigest}\")" bundles/redhat-marketplace/"$RELEASE"/manifests/"$package".clusterserviceversion.yaml
 
 # Use SHA Digest for DirectPV Image
