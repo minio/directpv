d8bd1bff2da4f95febdccd437d865b9710d345d2
Praveenrajmani <praveen@minio.io>
2023-07-17 12:16:13 +0530

    Upgrade the CSI sidecars to latest versions
    This patch upgrades the CSI sidecars to the following latest versions
    
    - quay.io/minio/csi-provisioner:v3.5.0
    - quay.io/minio/csi-node-driver-registrar:v2.8.0
    - quay.io/minio/livenessprobe:v2.10.0
    - quay.io/minio/csi-resizer:v1.8.0
    
    Note: These versions are compiled with go1.20 and should not have any go
    vulns reported.

--- pkg/installer/args.go
+++ pkg/installer/args.go
@@ -29,23 +29,23 @@ import (
 )
 
 const (
-	// csiProvisionerImage = csi-provisioner:v3.4.0
-	csiProvisionerImage          = "csi-provisioner@sha256:704fe68a6344774d4d0fde980af64fac2f2ddd27fb2e7f7c5b3d8fbddeae4ec8"
+	// csiProvisionerImage = csi-provisioner:v3.5.0
+	csiProvisionerImage          = "csi-provisioner@sha256:7b5c070ec70d30b0895d91b10c39a0e6cc81c18e0d1566c77aeff2a3587fa316"
 	openshiftCSIProvisionerImage = "registry.redhat.io/openshift4/ose-csi-external-provisioner@sha256:778aa6e5ea046bfcd865e665679c30362dc8c00cfb33a0cdcc56b2395e8ab504"
 
 	// csiProvisionerImageV2_2_0 = "csi-provisioner:v2.2.0-go1.18"
 	csiProvisionerImageV2_2_0 = "csi-provisioner@sha256:c185db49ba02c384633165894147f8d7041b34b173e82a49d7145e50e809b8d6"
 
-	// nodeDriverRegistrarImage = csi-node-driver-registrar:v2.6.3
-	nodeDriverRegistrarImage          = "csi-node-driver-registrar@sha256:68ee8f0b10acb4189e506d8a0e40c995362d886a35d5cbb17624e59913af0145"
+	// nodeDriverRegistrarImage = csi-node-driver-registrar:v2.8.0
+	nodeDriverRegistrarImage          = "csi-node-driver-registrar@sha256:c805fdc166761218dc9478e7ac8e0ad0e42ad442269e75608823da3eb761e67e"
 	openshiftNodeDriverRegistrarImage = "registry.redhat.io/openshift4/ose-csi-node-driver-registrar@sha256:0db5ea72a708503516f33f221848f0adaee71901769699ef5322a79e2da4f6d1"
 
-	// livenessProbeImage = livenessprobe:v2.9.0
-	livenessProbeImage          = "livenessprobe@sha256:0522eff1d8e9269655080500c1f6388fe2573978e8a74e08beaf3452cd575c2e"
+	// livenessProbeImage = livenessprobe:v2.10.0
+	livenessProbeImage          = "livenessprobe@sha256:f3bc9a84f149cd7362e4bd0ae8cd90b26ad020c2591bfe19e63ff97aacf806c3"
 	openshiftLivenessProbeImage = "registry.redhat.io/openshift4/ose-csi-livenessprobe@sha256:81f9f06a7de9a79013a4690ad616c1aff9638ab64284626491f44645a07051ec"
 
-	// csiResizerImage = csi-resizer:v1.7.0
-	csiResizerImage          = "csi-resizer@sha256:a88ca4a9bfbd2e604aedae5a04a5c180540259e3ab75393755ff73d587a619b2"
+	// csiResizerImage = csi-resizer:v1.8.0
+	csiResizerImage          = "csi-resizer@sha256:819f68a4daf75acec336302843f303cf360d4941249f9f5019ffbb690c8ac7c0"
 	openshiftCSIResizerImage = "registry.redhat.io/openshift4/ose-csi-external-resizer-rhel8@sha256:837b32a0c432123e2605ad6d029e7f3b4489d9c52a9d272c7a133d41ad10db87"
 )
 
