86f054e89dba0bd8e448c02416f769efb2ad4e63
Bala FA <bala@minio.io>
2025-04-24 08:05:49 +0530

    InitDevices: handle device denied reason and PartTableType (#1000)
    * Return error if denied device is passed
    * Use force flag if device PartTableType is set
    
    Signed-off-by: Bala.FA <bala@minio.io>

--- pkg/initrequest/event.go
+++ pkg/initrequest/event.go
@@ -191,15 +191,19 @@ func (handler *initRequestEventHandler) initDevices(ctx context.Context, req *ty
 		case !found:
 			results[i].Error = "device not found"
 		case device.ID(handler.nodeID) != req.Spec.Devices[i].ID:
-			results[i].Error = "device's state changed"
+			results[i].Error = "device state changed"
 		default:
-			wg.Add(1)
-			go func(i int, device pkgdevice.Device, force bool) {
-				defer wg.Done()
-				if err := handler.initDevice(device, force); err != nil {
-					results[i].Error = err.Error()
-				}
-			}(i, device, req.Spec.Devices[i].Force)
+			if deniedReason := device.DeniedReason(); deniedReason == "" {
+				wg.Add(1)
+				go func(i int, device pkgdevice.Device, force bool) {
+					defer wg.Done()
+					if err := handler.initDevice(device, force); err != nil {
+						results[i].Error = err.Error()
+					}
+				}(i, device, req.Spec.Devices[i].Force || device.PartTableType() != "")
+			} else {
+				results[i].Error = "device init not permitted; " + deniedReason
+			}
 		}
 	}
 	wg.Wait()
